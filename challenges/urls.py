507305e89368a8218bac51e73be98b641f01823f
Bhargodev Arya <bhargodevarya@gmail.com>
2021-05-14 16:11:30 +0530

    Returning response from a template

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+# Trailing slash is important, wont work without it in safari
+# Order of the paths is also important
+#
 urlpatterns = [
+    path("", views.home),
     path("aboutme/", views.index),
-    path("january/", views.january),
-    path("february/", views.february),
+    # path("january/", views.january),
+    # path("february/", views.february),
+    path("<int:month>", views.monthly_challenge_with_month_as_digit),
+    path("<str:month>", views.monthly_challenge),
     path("<fname>/<lname>", views.say_hello)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,22 @@
+from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 # Create your views here.
 
+month_dict = {
+    "january":"January challenge",
+    "february":"February challenge",
+    "march":"March challenge",
+    "april":"April challenge",
+    "may":"May challenge",
+    "june":"June challenge",
+    "july":"July challenge",
+    "august":"August challenge",
+    "september":"September challenge",
+    "october":"October challenge",
+    "november":"November challenge",
+    "december":"December challenge"
+}
 
 def index(request: HttpRequest):
     return HttpResponse("I am up for any challenge!")
@@ -15,3 +30,20 @@ def february(request):
 
 def say_hello(request, fname, lname):
     return HttpResponse("Hello " + fname + " " + lname)
+
+def home(request):
+    return render(request, "challenges/index.html")
+
+def monthly_challenge_with_month_as_digit(request, month):
+    if month < 0 or month > 12:
+        return HttpResponseNotFound("Invalid month")
+    return HttpResponse(month_dict[list(month_dict.keys())[month-1]])
+
+def monthly_challenge(request, month):
+    try:
+        response = month_dict[month.lower()]
+        return HttpResponse(response)
+    except:
+        return HttpResponseNotFound("Invalid month or no challenge found")
+
+
