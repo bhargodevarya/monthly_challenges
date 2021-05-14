6a4633809a35126be37ed8365cd14288ae36b438
Bhargodev Arya <bhargodevarya@gmail.com>
2021-05-14 13:27:49 +0530

    URL path variables

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("aboutme/", views.index)
+    path("aboutme/", views.index),
+    path("january/", views.january),
+    path("february/", views.february),
+    path("<fname>/<lname>", views.say_hello)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 # Create your views here.
 
 
-def index(request):
+def index(request: HttpRequest):
     return HttpResponse("I am up for any challenge!")
+
+def january(request):
+    request
+    return HttpResponse("This is the challenge for January")
+
+def february(request):
+    return HttpResponse("This is the challenge for February")
+
+def say_hello(request, fname, lname):
+    return HttpResponse("Hello " + fname + " " + lname)
