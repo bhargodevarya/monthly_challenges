60d93785f01295197afd0a030ed368c724f9a1d8
Bhargodev Arya <bhargodevarya@gmail.com>
2021-05-18 19:47:37 +0530

    Using tags for including partial pages, POST endpoint for creating a challenge, other misc changes

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,12 +5,13 @@ from . import views
 # Order of the paths is also important
 #
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="index"),
     path("aboutme/", views.index),
     # path("january/", views.january),
     # path("february/", views.february),
+    path("view/<str:month>", views.add_challenge_success),
     path("<int:month>", views.monthly_challenge_with_month_as_digit),
     path("add/<str:month>", views.add_challenge),
     path("<str:month>", views.monthly_challenge),
-    path("<fname>/<lname>", views.say_hello)
+    #path("<fname>/<lname>", views.say_hello)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponseNotFound
+from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 # Create your views here.
@@ -6,63 +6,83 @@ from django.http import HttpResponse, HttpRequest
 from . import models
 
 month_dict = {
-    "january":"January challenge",
-    "february":"February challenge",
-    "march":"March challenge",
-    "april":"April challenge",
-    "may":"May challenge",
-    "june":"June challenge",
-    "july":"July challenge",
-    "august":"August challenge",
-    "september":"September challenge",
-    "october":"October challenge",
-    "november":"November challenge",
-    "december":"December challenge"
+    "january": "January challenge",
+    "february": "February challenge",
+    "march": "March challenge",
+    "april": "April challenge",
+    "may": "May challenge",
+    "june": "June challenge",
+    "july": "July challenge",
+    "august": "August challenge",
+    "september": "September challenge",
+    "october": "October challenge",
+    "november": "November challenge",
+    "december": "December challenge"
 }
+
 
 def index(request: HttpRequest):
     return HttpResponse("I am up for any challenge!")
+
 
 def january(request):
     request
     return HttpResponse("This is the challenge for January")
 
+
 def february(request):
     return HttpResponse("This is the challenge for February")
+
 
 def say_hello(request, fname, lname):
     return HttpResponse("Hello " + fname + " " + lname)
 
+
 def home(request):
     return render(request, "challenges/index.html", {
-        "message" : "Click to view challenges",
+        "message": "Click to view challenges",
         "months": list(month_dict.keys())
     })
+
 
 def monthly_challenge_with_month_as_digit(request, month):
     if month < 0 or month > 12:
         return HttpResponseNotFound("Invalid month")
     return HttpResponse(month_dict[list(month_dict.keys())[month-1]])
 
+
 def monthly_challenge(request, month):
     try:
         response = month_dict[month.lower()]
-        return HttpResponse(response)
+        return render(request, "challenges/view_monthly_challenge.html", {
+        "month": month
+    })
     except:
         return HttpResponseNotFound("Invalid month or no challenge found")
 
-def add_challenge(request:HttpRequest, month):
+
+def add_challenge(request: HttpRequest, month):
     print(request.body)
     # for res in models.Challenge.scan():
     #     print(res)
-    
+
     # for th in models.Thread.scan():
     #     print(th.forum_name)
 
-    models.make_challenge()
+    #models.make_challenge()
 
     return render(request, "challenges/add_challenge.html", {
         "month": month
     })
 
 
+def add_challenge_success(request, month):
+    if request.method == 'POST':
+        challenge = request.POST['challenge']
+        print(f"For the month of {month} added challenge,  {challenge}")
+    return render(request, "challenges/add_challenge_success.html", {
+        "message": "Click to view challenges",
+        "months": list(month_dict.keys()),
+        "challenge_add_success": True,
+        "challenge_add_success_msg": f"Challenge added successfully for {month}"
+    })
