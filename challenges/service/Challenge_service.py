7fb1be76d100cef055152a856b7c6dd62138b919
Bhargodev Arya <bhargodevarya@gmail.com>
2021-05-31 20:34:16 +0530

    Updated add challenge view to show all added challenges, new module to handle operations for challenges

---
+++ challenges/service/Challenge_service.py
@@ -0,0 +1,4 @@
+# Save the current challenge and fetch all the challenges for the givem month
+def save_challenge(challenge):
+    print(f'Save the challenge {challenge}')
+    return challenge
--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,12 @@
+from challenges.service import Challenge_service
 from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 # Create your views here.
 
 #from . import models
+
+from . import service
 
 month_dict = {
     "january": "January challenge",
@@ -77,12 +80,15 @@ def add_challenge(request: HttpRequest, month):
 
 
 def add_challenge_success(request, month):
+    challenge = None
     if request.method == 'POST':
         challenge = request.POST['challenge']
         print(f"For the month of {month} added challenge,  {challenge}")
+        challenges = {"text": Challenge_service.save_challenge({challenge})}
     return render(request, "challenges/add_challenge_success.html", {
         "message": "Click to view challenges",
         "months": list(month_dict.keys()),
         "challenge_add_success": True,
-        "challenge_add_success_msg": f"Challenge added successfully for {month}"
+        "challenge_add_success_msg": f"Challenge added successfully for {month}",
+        "challenges":  [{"text": challenge}]
     })
